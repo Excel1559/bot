7e8372fcac608a95161d8f4c5f50e5df9d0a5190
Excel1559 <Reva8390@gmail.com>
2017-11-08 20:13:53 -0500

    herpderp

--- example.py
+++ example.py
@@ -7,6 +7,11 @@ from os import environ
 from flask import Flask
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
 sys.path.append(os.path.join(sys.path[0], 'src'))
@@ -117,8 +122,3 @@ while True:
 
     else:
         print("Wrong mode!")
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
